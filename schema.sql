85b6f5d9cfbc1a9408fb2b58f243546463609e9f
Udbhav Prasad <udbhav523@gmail.com>
2025-03-06 03:01:42 -0800

    Implement feed reader and persistence

--- schema.sql
+++ schema.sql
@@ -2,26 +2,26 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS report (
     id TEXT PRIMARY KEY,
-    creation_time DATETIME,
+    publish_time DATETIME,
     title TEXT UNIQUE,
     summary TEXT UNIQUE,
     upload_time DATETIME,
-    source INT8, -- enum
-    ipv4s BLOB, -- serialized list
-    ipv6s BLOB, -- serialized list
-    urls BLOB, -- serialized list
-    yara_rules BLOB, -- serialized list
-    cves BLOB, -- serialized list
-    sha256s BLOB, -- serialized list
-    md5s BLOB, -- serialized list
-    sha1s BLOB, -- serialized list
+    source TEXT,
+    ipv4s TEXT, -- csv list
+    ipv6s TEXT, -- csv list
+    urls TEXT, -- csv list
+    yara_rules TEXT, -- json list
+    cves TEXT, -- csv list
+    sha256s TEXT, -- csv list
+    md5s TEXT, -- csv list
+    sha1s TEXT, -- csv list
     mitre TEXT, -- json
-    report_type INT8, -- enum
+    report_type TEXT,
     web_url TEXT UNIQUE,
     report_data_url TEXT UNIQUE
 );
 
-CREATE INDEX IF NOT EXISTS report_creation_time_idx ON report (creation_time);
+CREATE INDEX IF NOT EXISTS report_publish_time_idx ON report (publish_time);
 CREATE INDEX IF NOT EXISTS report_source_idx ON report (source);
 
 COMMIT;

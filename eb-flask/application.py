6207fec2365e538a7fafa9c0187f0d2f5ecd410c
Udbhav Prasad <udbhav523@gmail.com>
2025-03-07 12:35:29 -0800

    Test database connection when flask app starts

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,34 +1,50 @@
+import boto3
+from botocore.exceptions import ClientError
 from flask import Flask
+import json
+import psycopg2 as pg
 
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
-
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
-
-# EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+def get_pg_secret():
+    secret_name = "rds!db-087d55ac-2a56-4890-84e2-077ddf7542c8"
+    region_name = "us-east-2"
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
 
-# run the app.
-if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    return json.loads(get_secret_value_response['SecretString'])
+
+def create_pg_conn():
+    pg_secret = get_pg_secret()
+    return pg.connect(
+        database="reports",
+        user=pg_secret["username"],
+        password=pg_secret["password"],
+        host="reports.c786ks06yy45.us-east-2.rds.amazonaws.com",
+        port=5432,
+    )
+
+@application.route("/")
+def hello_world():
+    try:
+        pg_conn = create_pg_conn()
+    except Exception as e:
+        return f"<p> Error connecting to database</p>"
+
+    return f"<p>Hello, {pg_conn.info.dbname}</p>"
+
+if __name__ == '__main__':
     application.debug = True
     application.run()
